d31b1d0657dba6535e527fdb4b49a7b6d153b5ee
Andrey Volk <andywolk@gmail.com>
2024-05-10 20:22:56 +0300

    rpi

--- scripts/packaging/fsget.sh
+++ scripts/packaging/fsget.sh
@@ -2,37 +2,42 @@
 
 TOKEN=$1
 RELEASE=$2
-#ARCH=$(dpkg --print-architecture)
+
 # Source the os-release file (assuming it exists)
 . /etc/os-release
 echo $ID
 echo $VERSION_CODENAME
-#echo $ARCH
 
 if [ "${ID,,}" = "debian" ]; then
+	ARCH=$(dpkg --print-architecture)
 	if [[ "${TOKEN}" == pat_* ]]; then
 		echo "Public FreeSWITCH"
 
-		rm -f /etc/apt/sources.list.d/freeswitch.list
-		apt-get update && apt-get install -y gnupg2 wget software-properties-common apt-transport-https
+		RPI=""
 
-		wget --http-user=signalwire --http-password=$TOKEN -O /usr/share/keyrings/signalwire-freeswitch-repo.gpg https://freeswitch.signalwire.com/repo/deb/debian-release/signalwire-freeswitch-repo.gpg
-		echo "machine freeswitch.signalwire.com login signalwire password $TOKEN" > /etc/apt/auth.conf
-		chmod 600 /etc/apt/auth.conf
 		if [ "${RELEASE,,}" = "unstable" ]; then
 			RELEASE="unstable"
 		else
 			RELEASE="release"
 		fi
-		echo "deb [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-${RELEASE}/ ${VERSION_CODENAME} main" > /etc/apt/sources.list.d/freeswitch.list
-		echo "deb-src [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-${RELEASE}/ ${VERSION_CODENAME} main" >> /etc/apt/sources.list.d/freeswitch.list
 
-		apt-get update
-	elif [[ "${TOKEN}" == fse_* ]]; then
-		echo "FreeSWITCH Enterprise"
+		if [ "${ARCH,,}" = "armhf" ]; then
+			RPI="rpi/"
+		fi
 
 		rm -f /etc/apt/sources.list.d/freeswitch.list
 		apt-get update && apt-get install -y gnupg2 wget software-properties-common apt-transport-https
+
+		wget --http-user=signalwire --http-password=$TOKEN -O /usr/share/keyrings/signalwire-freeswitch-repo.gpg https://freeswitch.signalwire.com/repo/deb/${RPI}debian-release/signalwire-freeswitch-repo.gpg
+		echo "machine freeswitch.signalwire.com login signalwire password $TOKEN" > /etc/apt/auth.conf
+		chmod 600 /etc/apt/auth.conf
+		echo "deb [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/${RPI}debian-${RELEASE}/ ${VERSION_CODENAME} main" > /etc/apt/sources.list.d/freeswitch.list
+		echo "deb-src [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/${RPI}debian-${RELEASE}/ ${VERSION_CODENAME} main" >> /etc/apt/sources.list.d/freeswitch.list
+
+		apt-get update
+		echo "To install FreeSWITCH type: apt-get install -y freeswitch-meta-all"
+	elif [[ "${TOKEN}" == fse_* ]]; then
+		echo "FreeSWITCH Enterprise"
 
 		if [ "${RELEASE,,}" = "unstable" ]; then
 			RELEASE="unstable"
@@ -40,13 +45,21 @@ if [ "${ID,,}" = "debian" ]; then
 			RELEASE="1.8"
 		fi
 
-		wget --http-user=signalwire --http-password=$TOKEN -O - https://fsa.freeswitch.com/repo/deb/fsa/pubkey.gpg | apt-key add -
+		if [ "${ARCH,,}" = "armhf" ]; then
+			RPI="-rpi"
+		fi
+
+		rm -f /etc/apt/sources.list.d/freeswitch.list
+		apt-get update && apt-get install -y gnupg2 wget software-properties-common apt-transport-https
+
+		wget --http-user=signalwire --http-password=$TOKEN -O - https://fsa.freeswitch.com/repo/deb/fsa${RPI}/pubkey.gpg | apt-key add -
 		echo "machine fsa.freeswitch.com login $USERNAME password $PASSWORD" > /etc/apt/auth.conf
 		chmod 600 /etc/apt/auth.conf
-		echo "deb https://fsa.freeswitch.com/repo/deb/fsa/ ${VERSION_CODENAME} ${RELEASE}" > /etc/apt/sources.list.d/freeswitch.list
-		echo "deb-src https://fsa.freeswitch.com/repo/deb/fsa/ ${VERSION_CODENAME} ${RELEASE}" >> /etc/apt/sources.list.d/freeswitch.list
+		echo "deb https://fsa.freeswitch.com/repo/deb/fsa${RPI}/ ${VERSION_CODENAME} ${RELEASE}" > /etc/apt/sources.list.d/freeswitch.list
+		echo "deb-src https://fsa.freeswitch.com/repo/deb/fsa${RPI}/ ${VERSION_CODENAME} ${RELEASE}" >> /etc/apt/sources.list.d/freeswitch.list
 
 		apt-get update
+		echo "To install FreeSWITCH type: apt-get install -y freeswitch-meta-all"
 	else
 		echo "Unrecognized token type"
 	fi
